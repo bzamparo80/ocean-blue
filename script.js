6d92f385b80e567b6d652a536101a9a2a9b4d373
Belen Zamparo <111774410+bzamparo80@users.noreply.github.com>
2023-11-29 15:03:47 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function watchForEntry(entries, observer) {
 const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 0, // El elemento debe estar completamente en el área visible
+    threshold: 0.01, // Cuanto del elemento tiene que estar visible para que se active
 };
 
 const observer = new IntersectionObserver(watchForEntry, options);
@@ -60,10 +60,10 @@ if (aboutSection) {
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top <= window.innerHeight &&
+      rect.bottom >= 0 &&
+      rect.left <= window.innerWidth &&
+      rect.right >= 0
     );
   }
   
@@ -74,11 +74,11 @@ function isElementInViewport(el) {
   
     if (isElementInViewport(servicesSection)) {
       boxes.forEach(function (box) {
-        box.classList.add('scale-up');
+        box.classList.add('scale-up-center');
       });
     } else {
       boxes.forEach(function (box) {
-        box.classList.remove('scale-up');
+        box.classList.remove('scale-up-center');
       });
     }
   }
